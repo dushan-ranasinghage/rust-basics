c0ec5fbce2068e4560f85c1ac33f058baa4fa0ea
Dushan Ranasinghage <dushanrandika1994@outlook.com>
2024-06-19 18:47:47 +1200

    Add hello world and rust gitignore

---
+++ hello.rs
@@ -0,0 +1,11 @@
+
+/**
+ * @file hello.rs
+ * @description
+ * @author Dushan Ranasinghage
+ * @copyright Copyright 2024 - ResearchIt All Rights Reserved.
+ */
+
+fn main() {
+    println!("Hello, World!");
+}
